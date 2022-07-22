4f9a668a914ab3ce9c0f3b745c125bd24dff3e13
Nicole <nlimtung@gmail.com>
2022-07-22 11:26:02 -0400

    making nav bar keyboard accesible

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ function myFunction() {
     // })
 
   }
+
+
+const topLevelLinks = document.querySelectorAll('.menu__link')
+
+topLevelLinks.forEach(link => {
+  if (link.nextElementSibling) {
+    link.addEventListener('focus', function() {
+      this.parentElement.classList.add('focus')
+    })
+
+    const subMenu = link.nextElementSibling
+    const subMenuLinks = subMenu.querySelectorAll('a')
+    const lastLinkIndex = subMenuLinks.length - 1
+    const lastLink = subMenuLinks[lastLinkIndex]
+
+    lastLink.addEventListener('blur', function() {
+      link.parentElement.classList.remove('focus')
+    })
+  }
+})
