0381eefe37298fc84c0b4cfeb954307f48212e9a
Nicole <nlimtung@gmail.com>
2022-07-23 20:58:24 -0400

    adding mouseover function

--- js/navbar-accesibility.js
+++ js/navbar-accesibility.js
@@ -1,10 +1,27 @@
 const topLevelLinks = document.querySelectorAll('.menu__link')
+const collapsibleNavBarLinks = document.getElementById('collapsible')
+const subNavClass = document.getElementsByClassName('subnav-menu')
+const menuLinkClass = document.getElementsByClassName('menu__link')
+
+
+// remove focus if hover away outside
+document.addEventListener('mouseover', function(event) {
+  if (event.target.id != 'collapsible') {
+    for(var i = 0; i < menuLinkClass.length; i++){
+      menuLinkClass[i].setAttribute('aria-expanded', 'false')
+    }
+    collapsibleNavBarLinks.setAttribute('aria-expanded', 'false')
+    for(var i = 0; i < subNavClass.length; i++){
+      subNavClass[i].classList.remove('focus')
+      }
+   }
+})
 
 
 topLevelLinks.forEach(link => {
   if (link.nextElementSibling) {
-
     // add focus and change aria expanded
+
     link.addEventListener('focus', function() {
       this.parentElement.classList.add('focus')  
       this.parentElement.firstElementChild.setAttribute("aria-expanded", "true")
@@ -15,9 +32,8 @@ topLevelLinks.forEach(link => {
     const lastLinkIndex = subMenuLinks.length - 1
     const lastLink = subMenuLinks[lastLinkIndex]
 
-    // remove previous linkfocus and eria expanded
+    // remove previous linkfocus and aria expanded
     lastLink.addEventListener('blur', function() {
-      console.log(lastLink.parentElement.parentElement.previousElementSibling.getAttribute("aria-expanded"))
       lastLink.parentElement.parentElement.previousElementSibling.setAttribute("aria-expanded", "false")
       link.parentElement.classList.remove('focus')
     })
