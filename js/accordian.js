f1f4f901f7c1efbceb5e09f28170d125f0ac83ad
Hasan Abdirahman <hasanabdirahman.ao@gmail.com>
2023-07-18 14:45:18 -0400

    made several changes

--- js/accordian.js
+++ js/accordian.js
@@ -1,18 +1,24 @@
 var $tabs = $(".tab");
 var $verticalTabs = $(".tab-vertical");
+var $size = $(".size");
 
-$tabs.click(function() {
+$tabs.click(function () {
   $(this).parent(".column").toggleClass("show");
   $tabs.not(this).parent().removeClass("show");
 });
 
-$verticalTabs.click(function() {
+$verticalTabs.click(function () {
+  $("#change").val("-");
+  $(this).parent(".slide").toggleClass("show");
+  $verticalTabs.css({ opacity: "0.3", color: black });
+  $verticalTabs.not(this).parent().removeClass("show");
+});
+
+$verticalTabs.focus(function () {
   $(this).parent(".slide").toggleClass("show");
   $verticalTabs.not(this).parent().removeClass("show");
 });
 
-$verticalTabs.focus(function() {
-  $(this).parent(".slide").toggleClass("show");
-  $verticalTabs.not(this).parent().removeClass("show");
+$verticalTabs.click(function () {
+  $size.val("-");
 });
-
