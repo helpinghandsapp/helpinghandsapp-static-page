0170e80773262fb542bb03b25db5e9416fca05f0
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2023-07-21 13:17:04 -0400

    Moved minified js code into their own files and ran prettier

--- footer.js
+++ footer.js
@@ -1,102 +1,176 @@
 function generateFooter() {
   return `
-  <footer>
-  <div class = "wrapper">
-        <div class = "footer-contents container-fluid row">
-            <!-- footer links -->
-            <!-- <div class = "footer-links col-sm-6"> -->
-              <ul class = "footer-links col-sm-2">
-                <li>
-                    <a href = "contactus.html">Contact Us</a>
+    <footer>
+      <div class="wrapper">
+        <div class="footer-contents container-fluid row">
+          <!-- footer links -->
+          <!-- <div class = "footer-links col-sm-6"> -->
+          <ul class="footer-links col-sm-2">
+            <li>
+              <a href="contactus.html">Contact Us</a>
+            </li>
+            <li>
+              <a href="jointeam.html">Join Team</a>
+            </li>
+            <li>
+              <a href="donate.html">Donate</a>
+            </li>
+            <li>
+              <a href="blog">Blog</a>
+            </li>
+          </ul>
+          <!-- </div> -->
+          <div class="footer-links col-sm-6">
+            <p class="footer-contents text-center">Funded & Supported By:</p>
+            <div class="footer-logo-container">
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/BlacktotheFuture-Logo-01.png"
+                    alt="Volunteer Fair Icon"
+                  />
+                </div>
+              </div>
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/laidlaw-big.png"
+                    alt="Volunteer Fair Icon"
+                  />
+                </div>
+              </div>
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/tf-desktop-logo.png"
+                    alt="Mentor Icon"
+                  />
+                </div>
+              </div>
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/partners-partenaires-white-en.png"
+                    alt="Council Icon"
+                  />
+                </div>
+              </div>
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/RHF_logo_pos_red.webp"
+                    alt="Sharing Stories Icon"
+                  />
+                </div>
+              </div>
+              <div class="footer-logo-item">
+                <div class="footer-feature-content">
+                  <img
+                    class="footer-logo-image"
+                    src="img/OTF-Logo_Horiz-White.png"
+                    alt="Entrepeneur Icon"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- footer social media  -->
+          <div class="footer-social-media col-sm-4">
+            <!-- <h2>We <i class="fa fa-heart"></i> New Friends</h2> -->
+            <p>
+              Want to collaborate, join the team, or just have general feedback?
+            </p>
+            <div class="social-media-icons">
+              <p>Give us a shout!</p>
+              <ul class="list-inline list-social">
+                <li class="social-media-icon">
+                  <a
+                    class="social-media-link"
+                    href="https://www.instagram.com/helpinghandsapp"
+                    target="_blank"
+                    aria-label="Find is on Instagram"
+                  >
+                    <i
+                      class="fab fa-instagram icon-size"
+                      aria-hidden="true"
+                      alt="Instagram Icon Logo"
+                    ></i>
+                  </a>
                 </li>
-                <li>
-                    <a href = "jointeam.html">Join Team</a>
+                <li class="social-media-icon">
+                  <a
+                    class="social-media-link"
+                    href="https://twitter.com/helpinghands"
+                    target="_blank"
+                    aria-label="Find is on Twitter"
+                  >
+                    <i
+                      class="fab fa-twitter-square icon-size"
+                      aria-hidden="true"
+                      alt="Twitter Icon Logo"
+                    ></i>
+                  </a>
                 </li>
-                <li>
-                    <a href = "donate.html">Donate</a>
+                <li class="social-media-icon">
+                  <a
+                    class="social-media-link"
+                    href="https://www.facebook.com/helphandsapp"
+                    target="_blank"
+                    aria-label="Find is on Facebook"
+                  >
+                    <i
+                      class="fab fa-facebook-square icon-size"
+                      aria-hidden="true"
+                      alt="Facebook Icon Logo"
+                    ></i>
+                  </a>
                 </li>
-                <li>
-                    <a href = "blog">Blog</a>
+                <li class="social-media-icon">
+                  <a
+                    class="social-media-link"
+                    href="https://www.linkedin.com/company/helpinghandsapp/"
+                    target="_blank"
+                    aria-label="Find is on LinkedIn"
+                  >
+                    <i
+                      class="fab fa-linkedin-square icon-size"
+                      aria-hidden="true"
+                      alt="LinkedIn Icon Logo"
+                    ></i>
+                  </a>
+                </li>
+                <li class="social-media-icon">
+                  <a
+                    class="social-media-link"
+                    href="#mailgo"
+                    data-address="info"
+                    data-domain="helpinghandsapp.com"
+                    aria-label="Email Us"
+                  >
+                    <i
+                      class="fa fa-envelope icon-size"
+                      aria-hidden="true"
+                      alt="Email Us"
+                    ></i>
+                  </a>
                 </li>
               </ul>
-            <!-- </div> -->
-            <div class = "footer-links col-sm-6">
-              <p class="footer-contents text-center">Funded & Supported By:</p>
-                <div class="footer-logo-container">
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/BlacktotheFuture-Logo-01.png" alt="Volunteer Fair Icon"/>
-                    </div>
-                  </div>
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/laidlaw-big.png" alt="Volunteer Fair Icon"/>
-                    </div>
-                  </div>
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/tf-desktop-logo.png" alt="Mentor Icon"/>
-                    </div>
-                  </div>
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/partners-partenaires-white-en.png" alt="Council Icon"/>
-                    </div>
-                  </div>
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/RHF_logo_pos_red.webp" alt="Sharing Stories Icon"/>
-                    </div>
-                  </div>
-                  <div class="footer-logo-item">
-                    <div class="footer-feature-content">
-                      <img class="footer-logo-image" src="img/OTF-Logo_Horiz-White.png" alt="Entrepeneur Icon"/>
-                    </div>
-                  </div>
-                </div>
             </div>
-            <!-- footer social media  -->
-            <div class = "footer-social-media col-sm-4">
-                <!-- <h2>We <i class="fa fa-heart"></i> New Friends</h2> -->
-                <p>Want to collaborate, join the team, or just have general feedback? </p>
-                <div class ="social-media-icons">
-                    <p>Give us a shout!</p>
-                        <ul class="list-inline list-social">
-                            <li class="social-media-icon">
-                                <a class="social-media-link" href="https://www.instagram.com/helpinghandsapp" target="_blank" aria-label = "Find is on Instagram">
-                                    <i class="fab fa-instagram icon-size" aria-hidden="true" alt = "Instagram Icon Logo"></i>
-                                </a>
-                            </li>
-                            <li class="social-media-icon">
-                                <a class="social-media-link" href="https://twitter.com/helpinghands" target="_blank" aria-label = "Find is on Twitter">
-                                    <i class="fab fa-twitter-square icon-size" aria-hidden="true" alt="Twitter Icon Logo" ></i>
-                                </a>
-                            </li>
-                            <li class="social-media-icon">
-                                <a class="social-media-link" href="https://www.facebook.com/helphandsapp" target="_blank" aria-label = "Find is on Facebook">
-                                    <i class="fab fa-facebook-square icon-size"aria-hidden="true" alt = "Facebook Icon Logo"></i>
-                                </a>
-                            </li>
-                            <li class="social-media-icon">
-                                <a class="social-media-link" href="https://www.linkedin.com/company/helpinghandsapp/" target="_blank" aria-label = "Find is on LinkedIn">
-                                    <i class="fab fa-linkedin-square icon-size"aria-hidden="true" alt = "LinkedIn Icon Logo" ></i>
-                                </a>
-                            </li>
-                            <li class="social-media-icon">
-                                <a class="social-media-link" href="#mailgo" data-address="info" data-domain="helpinghandsapp.com" aria-label = "Email Us">
-                                    <i class="fa fa-envelope icon-size" aria-hidden="true" alt = "Email Us"></i>
-                                </a>
-                            </li>
-                        </ul>
-                </div>
-            </div>
+          </div>
         </div>
 
         <!-- copyright -->
-    <div>
-        <p class="footer-copyright-text">&copy; 2023 Helping Hands. All Rights Reserved.</p> 
-    </div>
-  </div>     
-</footer>
+        <div>
+          <p class="footer-copyright-text">
+            &copy; 2023 Helping Hands. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   `;
 }
