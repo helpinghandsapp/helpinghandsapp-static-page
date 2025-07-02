256c1b1e4aaf4dd4d0240dc32ee092e90691f79f
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2025-07-02 12:19:44 -0400

    Footer copyright year and sponsor logo resize
    Also removed navbar.js which is unused. New footer script for data is added to every page. Error handling for loadContainers function.

--- js/load-containers.js
+++ js/load-containers.js
@@ -1,8 +1,11 @@
 function loadContainers(...containers) {
   containers.forEach((container, i) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", `../${container}.html`, false); // must be synchronous for bootstrap affix
-    xhr.send();
-    document.getElementById(`${container}-container`).innerHTML = xhr.responseText;
+    const containerElement = document.getElementById(`${container}-container`);
+    if (container) {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", `../${container}.html`, false); // must be synchronous for bootstrap affix
+      xhr.send();
+      containerElement.innerHTML = xhr.responseText;
+    }
   });
 }
