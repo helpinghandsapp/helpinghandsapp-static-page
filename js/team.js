edfbb4251a36d507476d905224651ca891d48de6
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2025-08-18 11:59:30 -0400

    Code styling and refactor
    Use a function to more efficiently generate HTML for the staff/volunteers, run prettier and delete unused loadmore.js.

--- js/team.js
+++ js/team.js
@@ -1,3 +1,43 @@
+function generateHTML(people, isCurrentStaff) {
+  return people
+    .map((person) => {
+      let contactInfo = "";
+      if (person.contactText && person.email) {
+        const [username, domain] = person.email.split("@");
+        contactInfo = `
+          <p class="about-us">
+            ${person.contactText}
+            <a
+              class="team-mailto-link"
+              href="#mailgo"
+              data-address="${username}"
+              data-domain="${domain}"
+              >${person.email}</a
+            >
+          </p>
+        `;
+      }
+      return `
+        <div class="${isCurrentStaff ? "team" : "volunteer"}-component">
+          <div class="${isCurrentStaff ? "main-team-photo" : "volunteer-image"}">
+            <img
+              class="headshot"
+              src="${person.image}"
+              alt="${person.imageAlt}"
+            />
+          </div>
+          <div class="${!isCurrentStaff ? "volunteer-" : ""}team-paragraph">
+            <h3 class="team-name">${person.name}</h3>
+            <p class="position-title">${person.position}</p>
+            <p class="about-us">${person.about}</p>
+            ${contactInfo}
+          </div>
+        </div>
+      `;
+    })
+    .join("");
+}
+
 fetch(
   "https://helping-hands-website.s3.ca-central-1.amazonaws.com/team/team.json",
 )
@@ -5,98 +45,19 @@ fetch(
   .then((data) => {
     $("#staff-container").html(
       data.staff.length > 0
-        ? data.staff
-            .map((person) => {
-              let contactInfo = "";
-              if (person.contactText && person.email) {
-                const [username, domain] = person.email.split("@");
-                contactInfo = `
-                  <p class="about-us">
-                    ${person.contactText}
-                    <a
-                      class="team-mailto-link"
-                      href="#mailgo"
-                      data-address="${username}"
-                      data-domain="${domain}"
-                      >${person.email}</a
-                    >
-                  </p>
-                `;
-              }
-              return `
-                <div class="team-component">
-                  <div class="main-team-photo">
-                    <img
-                      class="headshot"
-                      src="${person.image}"
-                      alt="${person.imageAlt}"
-                    />
-                  </div>
-                  <div class="team-paragraph">
-                    <h3 class="team-name">${person.name}</h3>
-                    <p class="position-title">${person.position}</p>
-                    <p class="about-us">${person.about}</p>
-                    ${contactInfo}
-                  </div>
-                </div>
-              `;
-            })
-            .join("")
+        ? generateHTML(data.staff, true)
         : "There are no staff members right now.",
     );
-
     $("#volunteers-container").html(
       data.volunteers.length > 0
-        ? data.volunteers
-            .map(
-              (person) => `
-                <div class="volunteer-component">
-                  <div class="volunteer-image">
-                    <img
-                      class="headshot"
-                      src="${person.image}"
-                      alt="${person.imageAlt}"
-                    />
-                  </div>
-                  <div class="volunteer-team-paragraph">
-                    <h3 class="team-name">${person.name}</h3>
-                    <p class="position-title">${person.position}</p>
-                    <p class="about-us">${person.about}</p>
-                  </div>
-                </div>
-              `,
-            )
-            .join("")
+        ? generateHTML(data.volunteers, false)
         : "There are no volunteers and placement students right now.",
     );
-
     $("#previous-container").html(
       data.previous.length > 0
-        ? data.previous
-            .map(
-              (person) => `
-                <div class="volunteer-component">
-                  <div class="volunteer-image">
-                    <img
-                      class="headshot"
-                      src="${person.image}"
-                      alt="${person.imageAlt}"
-                    />
-                  </div>
-                  <div class="volunteer-team-paragraph">
-                    <h3 class="team-name">${person.name}</h3>
-                    <p class="position-title">${person.position}</p>
-                    <p class="about-us">${person.about}</p>
-                  </div>
-                </div>
-              `,
-            )
-            .join("")
+        ? generateHTML(data.previous, false)
         : "There are no previous team members right now.",
     );
-
-    // Load accordion.js for showing and hiding PDF job posts
-    $.getScript("js/accordion.js");
   })
   .catch((err) => {
     console.error(err);
@@ -104,7 +65,7 @@ fetch(
       "There was a problem loading the staff members.",
     );
     $("#volunteers-container").html(
-      "There was a problem loading the volunteers and placement Students.",
+      "There was a problem loading the volunteers and placement students.",
     );
     $("#previous-container").html(
       "There was a problem loading the previous team members.",
